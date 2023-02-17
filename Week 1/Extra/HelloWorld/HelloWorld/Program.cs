50128c91744b5ab31feab52a59dd358ec07289fc
merlijn1411 <34530@ma-web.nl>
2023-02-17 13:04:29 +0100

    extra

---
+++ Week 1/Extra/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //maak een string
+            string text = "Hello, Wordl!";
+
+            //Hiermee kan je de kleur van de letters veranderen.
+            Console.ForegroundColor
+            = ConsoleColor.Green;
+
+            //om het woord te laten verschijnen moeten we een foreach loop maken
+            foreach (char letter in text)
+            {
+                Console.Write (letter);
+                Thread.Sleep(75);
+                //de Thread zorgt ervoor hoe snel het woord word uitgetyped.
+                //Hoe hoger het getal hoe langer hij er over doet en hoe kleinder het getal hoe sneller die het uit typed 
+            }
+            //Readkey zorgt ervoor dat die de loop gebruikt
+            Console.ReadKey();
+
+
+        }
+    }
+}
